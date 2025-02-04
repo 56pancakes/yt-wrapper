0515785b76f5197a358ae7120f81183d08d06a38
56pancakes <56p444@gmail.com>
2025-02-04 18:41:12 -0500

    fixed

--- test.js
+++
@@ -1,6 +0,0 @@
-const ytWrapper = require("./index.cjs");
-
-(async () => {
-  const videoData = await ytWrapper.getChannelFromID("UCBR8-60-B28hp2BmDPdntcQ", "AIzaSyB0_UWHLVeov-UZ1I-3l63c8-7CyX9vRMY");
-  console.log(videoData);
-})();
